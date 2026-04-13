c98221b5865192c35287cd838a9989102dfdc257
denhaqsyaha <denhaqsyaha@gmail.com>
2026-04-13 08:29:44 +0700

    Commit 1

--- soal1.c
+++ soal1.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 
-int main(void) {
+int main () {
+    int izin, suhu, radiasi, jam;
+
+    scanf("%d %d %d %d", &izin, &suhu, &radiasi, &jam);
+    
+    if (radiasi >= 6) {
+        printf("\nTOLAK");
+    } else if (suhu >= 390) {
+        printf("\nKARANTINA");
+    } else if (izin == 1 && (jam < 6 || jam > 20)) {
+        printf("\nTOLAK");
+    } else if (izin == 1) {
+        printf("\nMASUK");
+    } else if (izin == 2 && radiasi <= 2 && jam >= 8 && jam <= 20) {
+        printf("\nMASUK");
+    } else if (izin == 2) {
+        printf("\nPEMERIKSAAN");
+    } else if (izin == 3 && radiasi == 0 && suhu < 380) {
+        printf("\nMASUK");
+    } else {
+        printf("\nTOLAK");
+    }
+
     return 0;
 }
